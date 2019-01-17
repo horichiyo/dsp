5c1f87c5d3891c19cb86fa047ad23ce0cae5326a
yo <sun4geek@gmail.com>
2019-01-18 00:50:56 +0900

    グラフ表示
    グラフ表示

--- adapt/3/3.py
+++ adapt/3/3.py
@@ -1,10 +1,11 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
 fs = 10000 # サンプリング周波数
 fc = 2000  # カットオフ周波数
-N=7        # 入力点数
+N=10000       # 入力点数
 
 
 # 変数
@@ -30,10 +31,20 @@ x1 = y1 = 0.0
 output = np.array([])
 for j in range(N):
 	y0 = b0*input[j] + b1*x1 - a1*y1
-	print(y0)
 	x1 = input[j]
 	y1 = y0
 	output = np.append(output, y0)
 
 
-print(output)
+# print(output)
+
+plt.plot(np.arange(0,N), output)
+plt.xlim(0, 100)
+plt.show()
+
+amp = 20*np.log10(np.abs(np.fft.fft(output)))
+
+plt.plot(np.arange(0,N), amp)
+plt.xlim(0,N/2)
+plt.ylim(-50,0)
+plt.show()
